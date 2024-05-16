99932c3ab4614ab74dd74a2fae61615097978c53
gaescriba <gaescriba.trabajo@gmail.com>
2024-05-16 16:51:00 -0400

    feature: added initial deck

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 
+import makeDeck from './utils/makeDeck.js'
+
 function App() {
+
+  const [localDeck, useLocalDeck] = useState(makeDeck());
+
   const [count, setCount] = useState(0)
+
 
   return (
     <>
---
+++ src/utils/makeDeck.js
@@ -0,0 +1,68 @@
+const makeDeck = () => {
+
+  const intialDeck = [
+
+    //hearts
+    { suit: 'H' , value: "A"},
+    { suit: 'H' , value: "2"},
+    { suit: 'H' , value: "3"},
+    { suit: 'H' , value: "4"},
+    { suit: 'H' , value: "4"},
+    { suit: 'H' , value: "6"},
+    { suit: 'H' , value: "7"},
+    { suit: 'H' , value: "8"},
+    { suit: 'H' , value: "9"},
+    { suit: 'H' , value: "T"},
+    { suit: 'H' , value: "J"},
+    { suit: 'H' , value: "Q"},
+    { suit: 'H' , value: "K"},
+    //clubs
+    { suit: 'C' , value: "A"},
+    { suit: 'C' , value: "2"},
+    { suit: 'C' , value: "3"},
+    { suit: 'C' , value: "4"},
+    { suit: 'C' , value: "4"},
+    { suit: 'C' , value: "6"},
+    { suit: 'C' , value: "7"},
+    { suit: 'C' , value: "8"},
+    { suit: 'C' , value: "9"},
+    { suit: 'C' , value: "T"},
+    { suit: 'C' , value: "J"},
+    { suit: 'C' , value: "Q"},
+    { suit: 'C' , value: "K"},
+    //diamonds
+    { suit: 'D' , value: "A"},
+    { suit: 'D' , value: "2"},
+    { suit: 'D' , value: "3"},
+    { suit: 'D' , value: "4"},
+    { suit: 'D' , value: "4"},
+    { suit: 'D' , value: "6"},
+    { suit: 'D' , value: "7"},
+    { suit: 'D' , value: "8"},
+    { suit: 'D' , value: "9"},
+    { suit: 'D' , value: "T"},
+    { suit: 'D' , value: "J"},
+    { suit: 'D' , value: "Q"},
+    { suit: 'D' , value: "K"},
+    //spades
+    { suit: 'S' , value: "A"},
+    { suit: 'S' , value: "2"},
+    { suit: 'S' , value: "3"},
+    { suit: 'S' , value: "4"},
+    { suit: 'S' , value: "4"},
+    { suit: 'S' , value: "6"},
+    { suit: 'S' , value: "7"},
+    { suit: 'S' , value: "8"},
+    { suit: 'S' , value: "9"},
+    { suit: 'S' , value: "T"},
+    { suit: 'S' , value: "J"},
+    { suit: 'S' , value: "Q"},
+    { suit: 'S' , value: "K"},
+
+  ]
+
+  return intialDeck;
+
+};
+
+export default makeDeck;

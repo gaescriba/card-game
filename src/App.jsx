4b5efe48507525aec6c6bdef939a7fed8c0473e6
gaescriba <gaescriba.trabajo@gmail.com>
2024-05-16 18:31:32 -0400

    feature: added basic card visualization

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import makeDeck from './utils/makeDeck.js'
 
+import Card from './components/Card.jsx'
+
 function App() {
 
   const [localDeck, useLocalDeck] = useState(makeDeck());
@@ -10,9 +12,24 @@ function App() {
 
 
   return (
-    <>
-      Hello, cards!
-   </>
+    <div class="grid grid-cols-3 gap-5 items-center justify-center">
+      <div class="relative bg-green-300">
+        {
+          localDeck.map(card => {
+            const { suit, value } = card 
+            return(
+              <Card key = { localDeck.indexOf(card) } suitProp = { suit } valueProp = { value } />
+            )
+          })
+        }
+      </div>
+      <div class="bg-red-400">
+        Second deck
+      </div>
+      <div class="bg-blue-500">
+        Third deck
+      </div>
+   </div>
   )
 }
 
